e8ed6e7aeecfeb0284515a4041f6568ee54014e1
Chris Almeida <chris@gem.co>
2019-09-30 10:35:35 -0700

    export models from root

---
+++ src/models/index.ts
@@ -0,0 +1,19 @@
+import { FileModel, IFile } from './file';
+import { DocumentModel, IDocument } from './document';
+import {
+  ProfileModel,
+  IProfile,
+  IProfileAddress,
+  IProfileName,
+} from './profile';
+
+export {
+  FileModel,
+  IFile,
+  DocumentModel,
+  IDocument,
+  ProfileModel,
+  IProfile,
+  IProfileAddress,
+  IProfileName,
+};

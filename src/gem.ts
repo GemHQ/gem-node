7032df173680b5cb3e5fc6389204cde9ed168ef7
Chris Almeida <chris@gem.co>
2019-11-01 12:15:19 -0700

    expose Gem API errors in Gem root

--- src/gem.ts
+++ src/gem.ts
@@ -2,5 +2,6 @@ import 'es6-shim';
 import { Client } from './client';
 import { Endpoints } from './shared';
 import { SDK } from './sdk';
+import GemAPIError from './errors/gem_api';
 
-export { Client, Endpoints, SDK };
+export { Client, Endpoints, SDK, GemAPIError };

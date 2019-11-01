fb77c89b7590c2b6d5ee9d7961c73514a1837a42
Chris Almeida <chris@gem.co>
2019-11-01 12:32:25 -0700

    add http code to api error class

--- src/errors/gem_api.ts
+++ src/errors/gem_api.ts
@@ -1,12 +1,14 @@
 export default class GemAPIError extends Error {
   description: string;
   code: string;
+  status: number;
 
-  constructor(error: { description: string; code: string }) {
+  constructor(error: { description: string; code: string; status: number }) {
     super(error.code);
     // NOTE:  Allow extension of Error class once compiled.
     Object.setPrototypeOf(this, new.target.prototype);
     this.description = error.description;
     this.code = error.code;
+    this.status = error.status;
   }
 }

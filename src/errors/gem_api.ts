4fba46f15121d2a33cadfa24ef2e6ac478ae3baa
Chris Almeida <chris@gem.co>
2019-11-06 15:02:33 -0800

    add error from gem api response

--- src/errors/gem_api.ts
+++ src/errors/gem_api.ts
@@ -1,13 +1,22 @@
 export default class GemAPIError extends Error {
   description: string;
+  error: string;
+  // NOTE: code has been deprecated in favor of error
+  // but left for backwards compatibility.
   code: string;
   status: number;
 
-  constructor(error: { description: string; code: string; status: number }) {
-    super(error.code);
+  constructor(error: {
+    description: string;
+    error: string;
+    status: number;
+    code: string;
+  }) {
+    super(error.error || error.code);
     // NOTE:  Allow extension of Error class once compiled.
     Object.setPrototypeOf(this, new.target.prototype);
     this.description = error.description;
+    this.error = error.error;
     this.code = error.code;
     this.status = error.status;
   }

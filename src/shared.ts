1ded446aaeffd25496867c7ea287cea3408455b1
Chris Almeida <chris@gem.co>
2020-01-27 15:48:10 -0800

    add shared response types

--- src/shared.ts
+++ src/shared.ts
@@ -23,4 +23,149 @@ export enum Endpoints {
   transactions = '/transactions',
   credentials = '/credentials',
   connections = '/connections',
+  auth = '/auth',
+  otp = '/auth/otp',
+  session_validity = '/authenticated',
+}
+
+export namespace GemResponseType {
+  export interface ICreatedCredential {
+    credential_id: string;
+    credential_expires_at: string;
+  }
+  export interface ISessionValidity {
+    user_id: string;
+    application_id: string;
+    read_access_expires_at: string;
+    execute_access_expires_at: string;
+  }
+
+  export interface IConnection {
+    id: string;
+    created_at: string;
+    updated_at: string;
+    institution_id: string;
+    user_id: string;
+    institution_user_id: string;
+    status: string;
+    expires_at: string;
+    additional_reason: any;
+  }
+
+  export interface IDocument {
+    description: string;
+    type: string;
+    category: string;
+  }
+
+  export interface IUser {
+    id: string;
+    created_at: string;
+    updated_at: string;
+    verified: boolean;
+    consented: boolean;
+    connections?: IConnection[];
+    profiles?: IProfile[];
+  }
+
+  export interface IProfile {
+    id: string;
+    created_at: string;
+    updated_at: string;
+    user_id: string;
+    status: string;
+    expires_at: string;
+    documents: IDocument[];
+  }
+
+  export interface ICredentialFormat {
+    format: string;
+    fields: { [k: string]: any }[];
+  }
+
+  export interface IInstitution {
+    id: string;
+    name: string;
+    credentials_formats: ICredentialFormat[];
+  }
+
+  export interface IBaseMessage {
+    message: string;
+  }
+
+  export interface IVerifyOTP {
+    id: string;
+    created_at: string;
+    updated_at: string;
+    verified: boolean;
+    consented: boolean;
+  }
+
+  export interface IInstitutionUser {
+    id: string;
+    status: string;
+    profile_id: string;
+    institution_id: string;
+    connection_id: string;
+    external_id: string;
+    additional_reason: any;
+    profile: IProfile;
+    institution: IInstitution;
+    connection: IConnection;
+  }
+
+  export interface IAccount {
+    id: string;
+    created_at: string;
+    updated_at: string;
+    asset_id: string;
+    status: string;
+    external_id: string;
+    name: string;
+    type: string;
+    enabled: boolean;
+    connection_id: string;
+    institution_id: string;
+  }
+
+  export interface ITransactionFee {
+    type: string;
+    amount: number;
+    asset_id: string;
+    summary: string;
+    description: string;
+  }
+
+  export interface ITransaction {
+    id: string;
+    created_at: string;
+    updated_at: string;
+    user_id: string;
+    source: {
+      id: string;
+      created_at: string;
+      updated_at: string;
+      asset_id: string;
+      status: string;
+      external_id: string;
+      name: string;
+      type: string;
+      enabled: boolean;
+      connection_id: string;
+      institution_id: string;
+    };
+    fees: ITransactionFee[];
+    status: string;
+    source_amount: number;
+    destination_amount: number;
+    source_asset_id: string;
+    instructions: any;
+    destination_asset_id: string;
+    external_id: string;
+    type: string;
+    additional_data: any;
+    reason: {
+      message: string;
+    };
+  }
 }

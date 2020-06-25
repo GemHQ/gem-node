f40aba3025acf17b57885953bb9ce0e72cb7cf4d
Chris Almeida <chris@gem.co>
2020-06-25 14:52:49 -0600

    fix: deprecate the use of error code

--- src/errors/gem_api.ts
+++ src/errors/gem_api.ts
@@ -1,9 +1,6 @@
 export default class GemAPIError extends Error {
   description: string;
   error: string;
-  // NOTE: code has been deprecated in favor of error
-  // but left for backwards compatibility.
-  code: string;
   status: number;
   error_map?: { [key: string]: string };
 
@@ -11,15 +8,13 @@ export default class GemAPIError extends Error {
     description: string;
     error: string;
     status: number;
-    code: string;
     error_map?: { [key: string]: string };
   }) {
-    super(error.error || error.code);
+    super(error.error);
     // NOTE:  Allow extension of Error class once compiled.
     Object.setPrototypeOf(this, new.target.prototype);
     this.description = error.description;
     this.error = error.error;
-    this.code = error.code;
     this.status = error.status;
     this.error_map = error.error_map;
   }

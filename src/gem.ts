1ccb6259f76e956ed0e429e2df6e10a3ad9c79aa
Chris Almeida <chris@gem.co>
2020-07-20 13:04:24 -0600

    export Gem types

--- src/gem.ts
+++ src/gem.ts
@@ -1,7 +1,7 @@
 import 'es6-shim';
 import { Client } from './client';
-import { Endpoints, GemResponseType } from './shared';
+import { Endpoints, GemResponseType, GemTypes } from './shared';
 import { SDK } from './sdk';
 import GemAPIError from './errors/gem_api';
 
-export { Client, Endpoints, SDK, GemAPIError, GemResponseType };
+export { Client, Endpoints, SDK, GemAPIError, GemResponseType, GemTypes };

82e19ca26d7803e16ec80d9db3285c6b27c5b05d
Chris Almeida <chris@gem.co>
2020-07-27 14:28:49 -0600

    build and bump

--- dist/models/transaction.d.ts
+++ dist/models/transaction.d.ts
@@ -2,15 +2,17 @@ import { BlockchainAddress } from './shared';
 export interface ITransaction {
     source_id: string;
     source_amount: number;
+    source_asset_id?: string;
     type: string;
     preview?: boolean;
     blockchain_address: BlockchainAddress;
 }
 export declare class TransactionModel implements ITransaction {
     source_id: string;
+    source_asset_id?: string;
     source_amount: number;
     type: string;
     preview?: boolean;
     blockchain_address: BlockchainAddress;
-    constructor({ source_id, type, source_amount, preview, blockchain_address, }: ITransaction);
+    constructor({ source_id, source_asset_id, type, source_amount, preview, blockchain_address, }: ITransaction);
 }
--- dist/models/transaction.js
+++ dist/models/transaction.js
@@ -2,8 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var TransactionModel = (function () {
     function TransactionModel(_a) {
-        var source_id = _a.source_id, type = _a.type, source_amount = _a.source_amount, _b = _a.preview, preview = _b === void 0 ? false : _b, blockchain_address = _a.blockchain_address;
+        var source_id = _a.source_id, source_asset_id = _a.source_asset_id, type = _a.type, source_amount = _a.source_amount, _b = _a.preview, preview = _b === void 0 ? false : _b, blockchain_address = _a.blockchain_address;
         this.source_id = source_id;
+        this.source_asset_id = source_asset_id;
         this.type = type;
         this.source_amount = source_amount;
         this.preview = preview;

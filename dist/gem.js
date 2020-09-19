127b55941f000db90e3a171bc11e69e43840e428
Chris Almeida <chris@gem.co>
2020-09-18 19:01:55 -0600

    build

--- dist/gem.js
+++ dist/gem.js
@@ -1,6 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-require("es6-shim");
 var client_1 = require("./client");
 exports.Client = client_1.Client;
 var shared_1 = require("./shared");

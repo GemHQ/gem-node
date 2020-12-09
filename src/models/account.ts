3b36e7e888a0d150d383b9bc638c832844e89e9f
Chris Almeida <chris@gem.co>
2020-12-09 13:27:58 -0700

    add new card params type

--- src/models/account.ts
+++ src/models/account.ts
@@ -6,6 +6,7 @@ export enum AccountTypes {
   ExchangeAccount = 'ExchangeAccount',
   Wallet = 'Wallet',
   BlockchainAddress = 'BlockchainAddress',
+  WyreCardAccount = 'WyreDebitCardAccount',
 }
 
 export interface INewPlaidAccount {
@@ -32,4 +33,29 @@ export class PlaidAccountModel implements INewPlaidAccount {
     this.plaid_token = plaid_token;
     this.plaid_account_id = plaid_account_id;
   }
+}
+
+export interface INewCardParams {
+  first_name: string;
+  last_name: string;
+  address_street_1: string;
+  address_street_2: string;
+  address_country: string;
+  address_state: string;
+  address_city: string;
+  address_postal_code: string;
+  cc_number: string;
+  cc_verification_code: string;
+  cc_exp_month: string;
+  cc_exp_year: string;
+  cc_last_4: string;
+  cc_issuer: string;
+  phone_number: string;
+  email_address: string;
+}
+
+export interface INewWyreCardAccount {
+  type: AccountTypes.WyreCardAccount;
+  connection_id: string;
+  card_params: INewCardParams;
 }
